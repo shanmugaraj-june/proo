b1d5979ece7e1fe975148c1aa5d4fb1020087e8e
ShanmugaRaj <shanmugaraj4696@gmail.com>
2025-07-06 18:25:20 +0530

    Update index.js

--- src/componets/Home/index.js
+++ src/componets/Home/index.js
@@ -1,26 +1,35 @@
-import React from 'react'; 
-import {Link}  from "react-router-dom"
-import './index.css';
-import Header from '../Header'; 
+
+import Header from '../Header';  
+import About from "../About"; 
+import Service from "../Service"; 
+import Project from "../Project";  
+import Contact from "../Contact";  
+
+import './index.css' ;
+import { Link } from 'react-scroll';
 const Home = () => {
   return (
    <>
      <Header />
-      <section className="home">
+      <section className="home" id = "home">
       <div className="home-overlay">
         <div className="home-content">
           <h1 className="home-title">We Build Your Vision</h1>
-          <p className="home-subtitle">
+          <p className="home-subtitle"> 
             At Vinayaga Construction, we turn your dream structures into lasting reality.
           </p>
           <div className="home-buttons">
-            <Link to  = "/project" className = "Link"> <a className="btn primary">Explore Projects</a></Link>
-            <Link to  = "/contact"  className = "Link">  <a href="#contact" className="btn outline">Contact Us</a></Link>
+            <Link to ="project" smooth={true} duration={500}>  <a  className="btn primary">Explore Projects</a></Link>
+           <Link to = "contact" smooth={true} duration={500}> <a href="#contact" className="btn outline">Contact Us</a></Link>
           </div>
         </div>
-      </div>
-    </section>  
-   </>
+      </div> 
+    </section>   
+    <About />
+    <Service /> 
+    <Project/> 
+    <Contact /> 
+    </>
   );
 };
 
